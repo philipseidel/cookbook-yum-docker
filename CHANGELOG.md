e35141795a1659a9c48869940acfb37ebd547086
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-10-24 12:45:21 -0400

    edit CHANGELOG & bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ yum-docker CHANGELOG
 This file is used to list changes made in each version of the yum-docker
 cookbook.
 
+0.2.0
+-----
+- Sean OMeara (sean@chef.io)
+  - fix Chef 13 deprecation warnings
+
 0.1.0
 -----
 - St. Isidore de Seville (st.isidore.de.seville@gmail.com)
--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email 'st.isidore.de.seville@gmail.com'
 license 'MIT'
 description 'Installs/Configures yum Docker Vendor-Specific Repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 
 source_url 'https://github.com/st-isidore-de-seville/cookbook-yum-docker'
 issues_url 'https://github.com/st-isidore-de-seville/cookbook-yum-docker/issues'
 
 recipe 'yum-docker::default', 'Installs/Configures yum Docker Vendor-Specific Repository'
 
-depends 'yum', '~> 3.6'
+depends 'yum', '~> 3.8'
 
 supports 'redhat'
 supports 'centos'
